5f0b914d534e24ee83c2b288cf47cb09d26620e8
Himanshu <lodhihimanshu.dev@gmail.com>
2025-12-07 10:17:40 +0530

    Fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import { GoogleAnalytics } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
--- app/lib/analytics.ts
+++ app/lib/analytics.ts
@@ -2,7 +2,7 @@
 declare global {
     interface Window {
         gtag: (...args: any[]) => void;
-        dataLayer: any[];
+        // dataLayer: any[];
     }
 }
 
